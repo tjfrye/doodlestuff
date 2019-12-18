8762cde0728421130b6a2c242ca41d6f48f1ef3c
tjfrye <tjfrye@iastate.edu>
2019-12-18 03:12:28 -0600

    Modified main friends list activity to support fragments

--- app/src/main/java/edu/iastate/scribblestuff/FriendsActivity.java
+++ app/src/main/java/edu/iastate/scribblestuff/FriendsActivity.java
@@ -1,95 +1,48 @@
 package edu.iastate.scribblestuff;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+public class FriendsActivity extends AppCompatActivity {
 
-import java.util.ArrayList;
-
-public class FriendsListActivity extends AppCompatActivity {
-
-    private String TAG = "FriendsListActivity";
-    ArrayList<String> friendsList;
-    private TableLayout table;
-    private FirebaseAuth mAuth;
-    private FirebaseDatabase database;
-    private DatabaseReference databaseReference;
+    private String TAG = "FriendsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
 
-        table = findViewById(R.id.friendsListTable);
-
-        mAuth = FirebaseAuth.getInstance();
-        database = FirebaseDatabase.getInstance();
-        databaseReference = database.getReference("friendcodes");
-
-
-
-
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        Log.d(TAG, "UID is " + currentUser.getUid());
-
-
-
-
-
-
-
-        /*
-        Query query = myRef.orderByChild("user1").equalTo(currentUser.getDisplayName());
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot snap : dataSnapshot.getChildren()) {
-                    friendsList.add(snap.getKey());
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-            }
-        });
-
-         */
-
-
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.placeHolder, new FriendsListFragment()).commit();
     }
+
+    public void onFriendsListClicked(View view) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.placeHolder, new FriendsListFragment()).commit();
+    }
+
+    public void onPendingClicked(View view) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.placeHolder, new PendingFragment()).commit();
+    }
+
+    public void onAddFriendClicked(View view) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.placeHolder, new AddFriendFragment()).commit();
+    }
+
     /*
-    public Query getFriends() {
-
-    }
-    */
-
-    public void onFriendsListClicked() {
-
-    }
-
-    public void onPendingClicked() {
-
+    public void refresh(Frag) {
+        Fragment frag = getSupportFragmentManager().findFragmentByTag(tag);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.detach(frag);
+        ft.attach(frag);
+        ft.commit();
     }
 
-    public void onAddFriendClicked() {
-
-    }
-    
+     */
 }
